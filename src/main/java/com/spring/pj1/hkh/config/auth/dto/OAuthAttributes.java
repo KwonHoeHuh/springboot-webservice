aa1381bac7fd081690d8d40c0bf083f77f2dd3f0
KwonHoeHuh <iihihing@nate.com>
2023-02-24 17:29:57 +0900

    kakao commit

--- src/main/java/com/spring/pj1/hkh/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/spring/pj1/hkh/config/auth/dto/OAuthAttributes.java
@@ -30,6 +30,8 @@ public class OAuthAttributes {
                                      Map<String, Object> attributes) {
         if("naver".equals(registrationId))
             return ofNaver("id", attributes);
+        if("kakao".equals(registrationId))
+            return ofKakao("id", attributes);
 
         return ofGoogle(userNameAttributeName, attributes);
     }
@@ -52,6 +54,21 @@ public class OAuthAttributes {
                 .email((String) response.get("email"))
                 .picture((String) response.get("profile_image"))
                 .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+    private static OAuthAttributes ofKakao(String userNameAttributeName, Map<String, Object> attributes) {
+        // kakao는 kakao_account에 유저정보가 있다. (email)
+        Map<String, Object> kakaoAccount = (Map<String, Object>)attributes.get("kakao_account");
+        // kakao_account안에 또 profile이라는 JSON객체가 있다. (nickname, profile_image)
+        Map<String, Object> kakaoProfile = (Map<String, Object>)kakaoAccount.get("profile");
+
+        return OAuthAttributes.builder()
+                .name((String) kakaoProfile.get("nickname"))
+                .email((String) kakaoAccount.get("email"))
+                .picture((String) kakaoProfile.get("profile_image_url"))
+                .attributes(attributes)
                 .nameAttributeKey(userNameAttributeName)
                 .build();
     }
